88d56f87de85a9e5ab53bdb5fdc8c6f64a9bd8fc
dmAdmin <>
2019-02-20 17:06:17 +0000

    {/src/test/java/com/toyota/tfs/ScenarioJunitActivatorTest.java}

---
+++ src/test/java/com/toyota/tfs/ScenarioJunitActivatorTest.java
@@ -0,0 +1,7 @@
+package com.toyota.tfs;
+/**
+* Do not remove this file
+*/
+@org.junit.runner.RunWith(org.drools.workbench.screens.scenariosimulation.backend.server.runner.ScenarioJunitActivator.class)
+public class ScenarioJunitActivatorTest {
+}
