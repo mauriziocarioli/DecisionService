2bbf701723c222ac3d2fc3273c14565ffdeb6306
mauriziocarioli <maurizio.carioli@gmail.com>
2019-02-21 10:35:06 -0500

    removed references to test tech-preview

--- src/test/java/com/toyota/tfs/ScenarioJunitActivatorTest.java
+++
@@ -1,7 +0,0 @@
-package com.toyota.tfs;
-/**
-* Do not remove this file
-*/
-@org.junit.runner.RunWith(org.drools.workbench.screens.scenariosimulation.backend.server.runner.ScenarioJunitActivator.class)
-public class ScenarioJunitActivatorTest {
-}
